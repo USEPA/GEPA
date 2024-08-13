acfddd1e48a886a2a35b68c9b6d36107d4e39dcf
nkruskamp <nkruskamp@rti.org>
2024-08-13 15:25:48 -0400

    abd coal emi example

--- gch4i/emis_processing/task_abandoned_coal_emis.py
+++ gch4i/emis_processing/task_abandoned_coal_emis.py
@@ -1,81 +1,124 @@
+# %%
 from pathlib import Path
 from typing import Annotated
 
 import pandas as pd
-from pytask import Product, task, mark
+from pytask import Product, mark, task
 
-from gch4i.config import (emi_data_dir_path, ghgi_data_dir_path, max_year,
-                          min_year)
+from gch4i.config import (
+    V3_DATA_PATH,
+    emi_data_dir_path,
+    ghgi_data_dir_path,
+    max_year,
+    min_year,
+)
 from gch4i.utils import tg_to_kt
 
 
-@mark.persist
-@task(id="ab_coal_emi")
-def task_get_abd_coal_inv_data(
-    input_path: Path = ghgi_data_dir_path
-    / "coal/AbandonedCoalMines1990-2022_FRv1.xlsx",
-    output_path: Annotated[Path, Product] = emi_data_dir_path / "abd_coal_emi.csv",
-) -> None:
-    """read in the ghgi_ch4_kt values for each state"""
+def read_emi_data(in_path, src):
+    year_list = [str(x) for x in list(range(min_year, max_year + 1))]
 
     emi_df = (
         # read in the data
         pd.read_excel(
-            input_path,
+            in_path,
             sheet_name="InvDB",
             skiprows=15,
-            nrows=115,
-            usecols="A:BA",
+            # nrows=115,
+            # usecols="A:BA",
         )
         # name column names lower
         .rename(columns=lambda x: str(x).lower())
-        # drop columns we don't need
-        # # get just methane emissions
-        ## EEM: TODO - need to take the sum of liberated and recovered and used
-        ##  in otherwords, the net methane emissions is the methane
-        ## liberated minus the amount of methane recovered and used. 
-        .query("(ghg == 'CH4')")
-        .drop(
-            columns=[
-                "sector",
-                "category",
-                "subcategory1",
-                "subcategory2",
-                "subcategory3",
-                "subcategory4",
-                "subcategory5",
-                "carbon pool",
-                "fuel1",
-                "fuel2",
-                "exclude",
-                "id",
-                "sensitive (y or n)",
-                "data type",
-                "subsector",
-                "crt code",
-                "units",
-                "ghg",
-                "gwp",
-            ]
+        # format the names of the emission source group strings
+        .assign(
+            ghgi_source=lambda df: df["subcategory1"]
+            .astype(str)
+            .str.strip()
+            .str.casefold()
+        )
+        # rename the location column to what we need
+        .rename(columns={"georef": "state_code"})
+        # get just CH4 emissions, get only the emissions of our ghgi group
+        .query(f"(ghg == 'CH4') & (ghgi_source == '{src}')")
+        # get just the columns we need
+        .filter(
+            items=["state_code"] + year_list,
+            axis=1,
         )
         # set the index to state
-        .rename(columns={"georef": "state_code"})
         .set_index("state_code")
         # covert "NO" string to numeric (will become np.nan)
         .apply(pd.to_numeric, errors="coerce")
-        # drop states that have all nan values
+        # # drop states that have all nan values
         .dropna(how="all")
         # reset the index state back to a column
         .reset_index()
         # make the table long by state/year
         .melt(id_vars="state_code", var_name="year", value_name="ch4_tg")
+        # convert the units
         .assign(ghgi_ch4_kt=lambda df: df["ch4_tg"] * tg_to_kt)
+        # drop the old units
         .drop(columns=["ch4_tg"])
         # make the columns types correcet
         .astype({"year": int, "ghgi_ch4_kt": float})
+        # fill in missing values
         .fillna({"ghgi_ch4_kt": 0})
         # get only the years we need
         .query("year.between(@min_year, @max_year)")
+        # calculate a single value for each state/year
+        .groupby(["state_code", "year"])["ghgi_ch4_kt"]
+        .sum()
+        .reset_index()
     )
-    emi_df.to_csv(output_path, index=False)
+    return emi_df
 
+
+source_name = "abandoned_coal"
+
+proxy_file_path = V3_DATA_PATH.parents[1] / "gch4i_data_guide_v3.xlsx"
+
+proxy_data = pd.read_excel(proxy_file_path, sheet_name="testing").query(
+    f"gch4i_name == '{source_name}'"
+)
+
+emi_parameters_dict = {}
+for emi_name, data in proxy_data.groupby("emi"):
+    emi_parameters_dict[emi_name] = {
+        "input_paths": [ghgi_data_dir_path / source_name / x for x in data.file_name],
+        "source_list": data.ghgi_group.to_list(),
+        "output_path": emi_data_dir_path / f"{emi_name}.csv",
+    }
+emi_parameters_dict
+
+# input_paths, source_list, output_path = emi_parameters_dict["abd_coal_emi"].values()
+# display(input_paths, source_list, output_path)
+
+
+for _id, _kwargs in emi_parameters_dict.items():
+
+    @mark.persist
+    @task(id=_id, kwargs=_kwargs)
+    def task_and_coal_emi(
+        input_paths: list[Path],
+        source_list: list[str],
+        output_path: Annotated[Path, Product],
+    ) -> None:
+
+        source_list = [x.strip().casefold() for x in source_list]
+
+        emi_df_list = []
+        for input_path, ghgi_group in zip(input_paths, source_list):
+            individual_emi_df = read_emi_data(input_path, ghgi_group)
+            emi_df_list.append(individual_emi_df)
+
+        emission_group_df = (
+            pd.concat(emi_df_list)
+            .groupby(["state_code", "year"])["ghgi_ch4_kt"]
+            .sum()
+            .reset_index()
+        )
+        emission_group_df.head()
+        emission_group_df.to_csv(output_path)
+
+
+# %%
