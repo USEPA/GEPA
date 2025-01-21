918b96dfa0dcc416d524353b34b8448c996b840d
burnettear <burnettear@gmail.com>
2025-01-21 12:58:13 -0500

    Update task_rice

--- gch4i/emis_processing/task_rice_emi.py
+++ gch4i/emis_processing/task_rice_emi.py
@@ -1,3 +1,14 @@
+"""
+Name:                  3C Rice Cultivation Emissions
+Date Last Modified:    2025-01-21
+Authors Name:          Nick Kruskamp (RTI International)
+Purpose:               Clean and standardized rice cultivation emissions data
+Input Files:           - gch4i_data_guide_v3.xlsx
+                       - {V3_DATA_PATH}/ghgi/3C_rice_cultivation/Rice_90-22_State.xlsx
+Output Files:          - {emi_data_dir_path}/rice_cult_emi.csv
+"""
+
+# %% Import Libraries
 from pathlib import Path
 from typing import Annotated
 
@@ -14,99 +25,34 @@ from gch4i.config import (
 from gch4i.utils import tg_to_kt
 
 
-# %%
-def read_file(in_path, src):
-    year_list = [str(x) for x in list(range(min_year, max_year + 1))]
+# %% Initialize Parameters
+"""
+This section initializes the parameters for the task and stores them in the
+emi_parameters_dict.
 
-    emi_df = (
-        # read in the data
-        pd.read_excel(
-            in_path,
-            sheet_name="InvDB",
-            skiprows=15,
-            # nrows=115,
-            # usecols="A:BA",
-        )
-        # name column names lower
-        .rename(columns=lambda x: str(x).lower())
-        .assign(
-            ghgi_source=lambda df: df["category"].astype(str).str.strip().str.casefold()
-        )
-        .dropna(subset="ghgi_source")
-        # rename the location column to what we need
-        .rename(columns={"georef": "state_code"})
-        # get just CH4 emissions, get only the emissions of our ghgi group
-        .query(f"(ghg == 'CH4') & (ghgi_source == '{src}')")
-        # get just the columns we need
-        .filter(
-            items=["state_code"] + year_list,
-            axis=1,
-        )
-        .rename(columns={"georef": "state_code"})
-        .set_index("state_code")
-        # covert "NO" string to numeric (will become np.nan)
-        .apply(pd.to_numeric, errors="coerce")
-        # drop states that have all nan values
-        .dropna(how="all")
-        # reset the index state back to a column
-        .reset_index()
-        # make the table long by state/year
-        .melt(id_vars="state_code", var_name="year", value_name="ch4_tg")
-        .assign(ghgi_ch4_kt=lambda df: df["ch4_tg"] * tg_to_kt)
-        .drop(columns=["ch4_tg"])
-        # make the columns types correcet
-        .astype({"year": int, "ghgi_ch4_kt": float})
-        .fillna({"ghgi_ch4_kt": 0})
-        # get only the years we need
-        .query("year.between(@min_year, @max_year)")
-        # calculate a single value for each state/year
-        # NOTE: applies when more than 1 source are being combined together.
-        # otherwise has no effect.
-        .groupby(["state_code", "year"])["ghgi_ch4_kt"]
-        .sum()
-        .reset_index()
-    )
-    return emi_df
-
-
-# %%
-source_name = "rice_cultivation"
-
+The parameters are read from the emi_proxy_mapping sheet of the gch4i_data_guide_v3.xlsx
+file. The parameters are used to create the pytask task for the emi.
+"""
+# gch4i_name to filter the data guide
+source_name = "3C_rice_cultivation"
+# data guide directory
 proxy_file_path = V3_DATA_PATH.parents[1] / "gch4i_data_guide_v3.xlsx"
-
-proxy_data = pd.read_excel(proxy_file_path, sheet_name="testing").query(
+# read and query for the source name (gch4i_name)
+proxy_data = pd.read_excel(proxy_file_path, sheet_name="emi_proxy_mapping").query(
     f"gch4i_name == '{source_name}'"
 )
-proxy_data
 
+# initialize the emi_parameters_dict
 emi_parameters_dict = {}
-
-for emi_name, data in proxy_data.groupby("emi"):
+# loop through the proxy data and store the parameters in the emi_parameters_dict
+for emi_name, data in proxy_data.groupby("emi_id"):
     emi_parameters_dict[emi_name] = {
         "input_path": ghgi_data_dir_path / source_name / data.file_name.iloc[0],
-        "source_list": data.ghgi_group.to_list(),
+        "source_list": data.gch4i_source.to_list(),
         "output_path": emi_data_dir_path / f"{emi_name}.csv",
     }
 
-emi_parameters_dict
-
-input_path, source_list, output_path = emi_parameters_dict["rice_cult_emi"].values()
-# display(input_path.exists(), source_list, output_path)
-# %%
-"""
-for emissions_group in proxy_data:  <- emi column
-    create emissions group list
-    for subsource in emissions_group: <- ghgi_group column
-        calculate subsource emissions <- input file, sheet name(?), keyword, kwargs(?)
-        add to emissions group list
-    calculate combined emissions group
-    write emissions group file <- emi column value .csv
-"""
-
-
-# %%
-
-
+# %% Create Pytask Function and Loop
 for _id, _kwargs in emi_parameters_dict.items():
 
     @mark.persist
@@ -116,8 +62,20 @@ for _id, _kwargs in emi_parameters_dict.items():
         source_list: str,
         output_path: Annotated[Path, Product],
     ) -> None:
+        """read in the ghgi_ch4_kt values for each state
 
+        Args:
+            input_path (Path): Path to the input data file.
+            source_list (str): List of source names to extract from the data.
+            output_path (Annotated[Path, Product]): Path to the output data file.
+
+        Returns:
+            pd.DataFrame: Processed emissions data with columns [state_code, year,
+            ghgi_ch4_kt]
+        """
+        # Clean the source list
         source_list = [x.strip().casefold() for x in source_list]
+        # Define the years of interest
         year_list = [str(x) for x in list(range(min_year, max_year + 1))]
 
         emi_df = (
@@ -131,6 +89,7 @@ for _id, _kwargs in emi_parameters_dict.items():
             )
             # name column names lower
             .rename(columns=lambda x: str(x).lower())
+            # format the names of emission source group strings
             .assign(
                 ghgi_source=lambda df: df["category"]
                 .astype(str)
@@ -149,15 +108,18 @@ for _id, _kwargs in emi_parameters_dict.items():
             )
             .rename(columns={"georef": "state_code"})
             .set_index("state_code")
-            # covert "NO" string to numeric (will become np.nan)
+            # Convert NO/0 to NA, remove states with all 0 years
+            .replace(0, pd.NA)
             .apply(pd.to_numeric, errors="coerce")
-            # drop states that have all nan values
             .dropna(how="all")
+            .fillna(0)
             # reset the index state back to a column
             .reset_index()
             # make the table long by state/year
             .melt(id_vars="state_code", var_name="year", value_name="ch4_tg")
+            # convert units to kt
             .assign(ghgi_ch4_kt=lambda df: df["ch4_tg"] * tg_to_kt)
+            # drop tg units column
             .drop(columns=["ch4_tg"])
             # make the columns types correcet
             .astype({"year": int, "ghgi_ch4_kt": float})
@@ -171,6 +133,5 @@ for _id, _kwargs in emi_parameters_dict.items():
             .sum()
             .reset_index()
         )
-        emi_df
-        # %%
+        # write out data to outoput path
         emi_df.to_csv(output_path, index=False)
