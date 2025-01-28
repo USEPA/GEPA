700e3be2a8597e35560007f8a29c91fdc4779603
burnettear <burnettear@gmail.com>
2025-01-28 08:55:54 -0500

    Code Review: Petrochemicals Emi

--- gch4i/emis_processing/task_petrochemicals_emis.py
+++ gch4i/emis_processing/task_petrochemicals_emis.py
@@ -1,77 +1,155 @@
+"""
+Name:                   task_petrochemicals_emis.py
+Date Last Modified:     2024-12-16
+Authors Name:           A. Burnette (RTI International)
+Purpose:                Mapping of petrochemicals emissions to State, Year, emissions
+                        format
+gch4i_name:             2B8_petrochemicals
+Input Files:            - {ghgi_data_dir_path}/{2B8_petrochemicals}/
+                            State_Petrochemicals_1990-2022.xlsx
+Output Files:           - {emi_data_dir_path}/petrochemicals_emi.csv
+"""
+# %% STEP 0. Load packages, configuration files, and local parameters ------------------
 from pathlib import Path
 from typing import Annotated
-
-import pandas as pd
 from pytask import Product, task, mark
 
-from gch4i.config import (emi_data_dir_path, ghgi_data_dir_path, max_year,
-                          min_year)
+import pandas as pd
+
+from gch4i.config import (
+    V3_DATA_PATH,
+    emi_data_dir_path,
+    ghgi_data_dir_path,
+    max_year,
+    min_year
+    )
+
 from gch4i.utils import tg_to_kt
 
+# %% Step 1. Function to read in the petrochemicals emissions data ---------------------
 
-@mark.persist
-@task(id="petrochemicals_emi")
-def task_get_petrochemicals_inv_data(
-    input_path: Path = ghgi_data_dir_path
-    / "industry/State_Petrochemicals_1990-2022.xlsx",
-    output_path: Annotated[Path, Product] = emi_data_dir_path / "petrochemicals_emi.csv",
-) -> None:
-    """read in the ghgi_ch4_kt values for each state"""
 
+def get_petrochemicals_inv_data(in_path, src):
+    """
+    read in the ghgi_ch4_kt values for each state
+
+    Parameters
+    ----------
+    in_path : str
+        path to the input file
+    src : str
+        subcategory of interest
+    params : dict
+        additional parameters
+    """
+
+    # Read in the data
+    emi_df = pd.read_excel(
+        in_path,
+        sheet_name="InvDB",
+        skiprows=15,
+        nrows=457,
+        )
+    # Specify years to keep
+    year_list = [str(x) for x in list(range(min_year, max_year + 1))]
+    # Clean and format the data
     emi_df = (
-        # read in the data
-        pd.read_excel(
-            input_path,
-            sheet_name="InvDB",
-            skiprows=15,
-            nrows=457,
-            usecols="A:BA",
+        # Rename columns
+        emi_df.rename(columns=lambda x: str(x).lower())
+        .assign(
+            ghgi_source=lambda df: df["subcategory1"]
+            .astype(str)
+            .str.strip()
+            .str.casefold()
         )
-        # name column names lower
-        .rename(columns=lambda x: str(x).lower())
-        # drop columns we don't need
-        # # get just methane emissions
-        .query("(ghg == 'CH4') & (subcategory1.str.contains('Acrylonitrile'))")
-        .drop(
-            columns=[
-                "sector",
-                "category",
-                "subcategory1",
-                "subcategory2",
-                "subcategory3",
-                "subcategory4",
-                "subcategory5",
-                "carbon pool",
-                "fuel1",
-                "fuel2",
-                "exclude",
-                "id",
-                "sensitive (y or n)",
-                "data type",
-                "subsector",
-                "crt code",
-                "units",
-                "ghg",
-                "gwp",
-            ]
-        )
-        # set the index to state
         .rename(columns={"georef": "state_code"})
+        # Query for CH4 emissions and the source of interest
+        .query(f"(ghg == 'CH4') & (ghgi_source == '{src}')")
+        # Filter state code and years
+        .filter(items=["state_code"] + year_list, axis=1)
         .set_index("state_code")
-        # covert "NO" string to numeric (will become np.nan)
+        # Replace NA values with 0
+        .replace(0, pd.NA)
         .apply(pd.to_numeric, errors="coerce")
-        # drop states that have all nan values
         .dropna(how="all")
-        # reset the index state back to a column
+        .fillna(0)
         .reset_index()
-        # make the table long by state/year
+        # Melt the data: unique state/year
         .melt(id_vars="state_code", var_name="year", value_name="ch4_tg")
+        # Convert tg to kt
         .assign(ghgi_ch4_kt=lambda df: df["ch4_tg"] * tg_to_kt)
         .drop(columns=["ch4_tg"])
-        # make the columns types correcet
         .astype({"year": int, "ghgi_ch4_kt": float})
         .fillna({"ghgi_ch4_kt": 0})
-        # get only the years we need
+        # Ensure only years between min_year and max_year are included
         .query("year.between(@min_year, @max_year)")
-    )
-    emi_df.to_csv(output_path, index=False)
+        # Ensure state/year grouping is unique
+        .groupby(["state_code", "year"])["ghgi_ch4_kt"]
+        .sum()
+        .reset_index()
+        )
+    return emi_df
+
+
+# %% STEP 2. Initialize Parameters
+"""
+This section initializes the parameters for the task and stores them in the
+emi_parameters_dict.
+
+The parameters are read from the emi_proxy_mapping sheet of the gch4i_data_guide_v3.xlsx
+file. The parameters are used to create the pytask task for the emi.
+"""
+# gch4i_name in gch4i_data_guide_v3.xlsx, emi_proxy_mapping sheet
+source_name = "2B8_petrochemicals"
+# Directory name for GHGI data
+source_path = "2B8_petrochemicals"
+
+# Data Guide Directory
+proxy_file_path = V3_DATA_PATH.parents[1] / "gch4i_data_guide_v3.xlsx"
+# Read and query for the source name (ghch4i_name)
+proxy_data = pd.read_excel(proxy_file_path, sheet_name="emi_proxy_mapping").query(
+    f"gch4i_name == '{source_name}'"
+)
+
+# Initialize the emi_parameters_dict
+emi_parameters_dict = {}
+# Loop through the proxy data and store the parameters in the emi_parameters_dict
+for emi_name, data in proxy_data.groupby("emi_id"):
+    emi_parameters_dict[emi_name] = {
+        "input_paths": [ghgi_data_dir_path / source_path / x for x in data.file_name],
+        "source_list": [x.strip().casefold() for x in data.Subcategory1.to_list()],
+        "output_path": emi_data_dir_path / f"{emi_name}.csv"
+    }
+
+emi_parameters_dict
+
+
+# %% STEP 3. Create Pytask Function and Loop
+
+for _id, _kwargs in emi_parameters_dict.items():
+
+    @mark.persist
+    @task(id=_id, kwargs=_kwargs)
+    def task_petrochemicals_emi(
+        input_paths: list[Path],
+        source_list: list[str],
+        output_path: Annotated[Path, Product],
+    ) -> None:
+
+        # Initialize the emi_df_list
+        emi_df_list = []
+        # Loop through the input paths and source list to get the emissions data
+        for input_path, ghgi_group in zip(input_paths, source_list):
+            individual_emi_df = get_petrochemicals_inv_data(input_path,
+                                                            ghgi_group)
+            emi_df_list.append(individual_emi_df)
+
+        # Concatenate the emissions data and group by state and year
+        emission_group_df = (
+            pd.concat(emi_df_list)
+            .groupby(["state_code", "year"])["ghgi_ch4_kt"]
+            .sum()
+            .reset_index()
+        )
+        # Save the emissions data to the output path
+        emission_group_df.to_csv(output_path)
