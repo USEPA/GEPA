26fb6ded20a447c14375c35630ae0ccd9d10fe5f
burnettear <131624863+burnettear@users.noreply.github.com>
2025-01-30 21:11:54 -0500

    Update task_ng_all_well_count_proxy.py
    Added Header. Removed unused packages. Made structural adjustments.

--- gch4i/proxy_processing/task_ng_all_well_count_proxy.py
+++ gch4i/proxy_processing/task_ng_all_well_count_proxy.py
@@ -1,31 +1,32 @@
-# %%
+"""
+Name:                   task_ng_all_well_count_proxy.py.py
+Date Last Modified:     2025-01-30
+Authors Name:           Hannah Lohman (RTI International)
+Purpose:                Mapping of natural gas well count proxy emissions
+Input Files:            -
+Output Files:           -
+"""
+
+# %% Import Libriaries
 from pathlib import Path
 import os
 from typing import Annotated
-from zipfile import ZipFile
-import calendar
-import datetime
 
-from pyarrow import parquet
+
 import pandas as pd
-import osgeo
+
 import geopandas as gpd
 import numpy as np
-import seaborn as sns
-import shapefile as shp
+
 from pytask import Product, task, mark
 
 from gch4i.config import (
-    V3_DATA_PATH,
     proxy_data_dir_path,
     global_data_dir_path,
     sector_data_dir_path,
-    max_year,
-    min_year,
-    years,
+    years
 )
 
-from gch4i.utils import us_state_to_abbrev
 from gch4i.proxy_processing.ng_oil_production_utils import (
     calc_enverus_rel_emi,
     enverus_df_to_gdf,
@@ -35,16 +36,18 @@ from gch4i.proxy_processing.ng_oil_production_utils import (
     get_raw_NEI_data,
 )
 
-# %%
+# %% Pytask Functions
+
+
 @mark.persist
 @task(id="ng_all_well_count_proxy")
 def task_get_ng_all_well_count_proxy_data(
     state_path: Path = global_data_dir_path / "tl_2020_us_state.zip",
     enverus_production_path: Path = sector_data_dir_path / "enverus/production",
-    intermediate_outputs_path: Path = enverus_production_path / "intermediate_outputs",
+    intermediate_outputs_path: Path = sector_data_dir_path / "enverus/production/intermediate_outputs",
     nei_path: Path = sector_data_dir_path / "nei_og",
     all_well_count_output_path: Annotated[Path, Product] = proxy_data_dir_path / "ng_all_well_count_proxy.parquet",
-    ):
+):
     """
     Data come from Enverus, both Drilling Info and Prism
     The reason 2 datasets are used is because Prism does not include all states
@@ -78,27 +81,36 @@ def task_get_ng_all_well_count_proxy_data(
         .to_crs(4326)
     )
 
-    # Make Annual gridded arrays (maps) of well data (a well will be counted every month if there is any production that year)
-    # Includes NA Gas Wells and Production onshore in the CONUS region
-    # source emissions are related to the presence of a well and its production status (no emission if no production)
-    # Details: ERG does not include a well in the national count if there is no (cummulative) oil or gas production from that well.
-    # Wells are not considered active for a given year if there is no production data that year
-    # This may cause wells that are coadmpleted but not yet producing to be dropped from the national count. 
-    # ERG has developed their own logic to determine if a well is an HF well or not and that result is included in the 
-    # HF variable in this dataset. This method does not rely on the Enverus well 'Producing Status'
-    # Well Type (e.g., non-associated gas well) is determined based on annual production GOR at that well (CUM OIL/ CUM GAS), 
-    # but the presence of a well will only be included in maps in months where monthly gas prod > 0
+    """
+    Make Annual gridded arrays (maps) of well data (a well will be counted every
+        month if there is any production that year)
+    Includes NA Gas Wells and Production onshore in the CONUS region
+    source emissions are related to the presence of a well and its production status
+        (no emission if no production)
+    Details: ERG does not include a well in the national count if there is no
+        (cummulative) oil or gas production from that well.
+    Wells are not considered active for a given year if there is no production data that
+        year
+    This may cause wells that are coadmpleted but not yet producing to be dropped from
+        the national count.
+    ERG has developed their own logic to determine if a well is an HF well or not and
+        that result is included in the HF variable in this dataset. This method does not
+        rely on the Enverus well 'Producing Status'
+    Well Type (e.g., non-associated gas well) is determined based on annual production
+        GOR at that well (CUM OIL/ CUM GAS), but the presence of a well will only be
+        included in maps in months where monthly gas prod > 0
+    """
 
     # Proxy Data Dataframes:
     all_well_count_df = pd.DataFrame()  # Active gas well (conventional + HF) counts in a given month
 
     ## Enverus DI and Prism Data: 
-    # Read in and query formatted and corrrected Enverus data to create dictionaries of 
+    # Read in and query formatted and corrrected Enverus data to create dictionaries of
     # proxy data (Enverus data is from task_enverus_di_prism_data_processing.py)
     for iyear in years:
         enverus_file_name_iyear = f"formatted_raw_enverus_tempoutput_{iyear}.csv"
         enverus_file_path_iyear = os.path.join(intermediate_outputs_path, enverus_file_name_iyear)
-        ng_data_temp = (pd.read_csv(enverus_file_path_iyear, dtype={3:'str', 'spud_year': str, 'first_prod_year': str})
+        ng_data_temp = (pd.read_csv(enverus_file_path_iyear, dtype={3: 'str', 'spud_year': str, 'first_prod_year': str})
                         .query("STATE_CODE.isin(@state_gdf['state_code'])")
                         .query("OFFSHORE == 'N'")
                         .query("CUM_GAS > 0")
@@ -110,7 +122,7 @@ def task_get_ng_all_well_count_proxy_data(
                         )
 
         # Include wells in map only for months where there is gas production (emissions ~ when production is occuring)
-        for imonth in range(1,13):
+        for imonth in range(1, 13):
             imonth_str = f"{imonth:02}"  # convert to 2-digit months
             year_month_str = str(iyear)+'-'+imonth_str
             gas_prod_str = 'GASPROD_'+imonth_str
@@ -120,24 +132,25 @@ def task_get_ng_all_well_count_proxy_data(
                                    .assign(year_month=str(iyear)+'-'+imonth_str)
                                    )
             ng_data_imonth_temp = (ng_data_imonth_temp[[
-                'year', 'year_month','STATE_CODE','AAPG_CODE_ERG','LATITUDE','LONGITUDE',
-                'HF','WELL_COUNT',gas_prod_str,
-                'comp_year_month','spud_year','first_prod_year']]
+                'year', 'year_month', 'STATE_CODE', 'AAPG_CODE_ERG', 'LATITUDE',
+                'LONGITUDE', 'HF', 'WELL_COUNT', gas_prod_str, 'comp_year_month',
+                'spud_year', 'first_prod_year']]
                 )
             # All Gas Well Count
-            all_well_count_imonth = (ng_data_imonth_temp[['year','year_month','STATE_CODE','LATITUDE','LONGITUDE','WELL_COUNT']]
-                                    .rename(columns=lambda x: str(x).lower())
-                                    .rename(columns={"well_count":"proxy_data"})
-                                    .reset_index(drop=True)
+            all_well_count_imonth = (
+                ng_data_imonth_temp[['year', 'year_month', 'STATE_CODE', 'LATITUDE', 'LONGITUDE', 'WELL_COUNT']]
+                .rename(columns=lambda x: str(x).lower())
+                .rename(columns={"well_count": "proxy_data"})
+                .reset_index(drop=True)
                                     )
-            all_well_count_df = pd.concat([all_well_count_df,all_well_count_imonth])
+            all_well_count_df = pd.concat([all_well_count_df, all_well_count_imonth])
 
     # Delete unused temp data
     del ng_data_temp
     del ng_data_imonth_temp
     del all_well_count_imonth
 
-    # Calculate relative emissions and convert to a geodataframe 
+    # Calculate relative emissions and convert to a geodataframe
     all_well_count_df = calc_enverus_rel_emi(all_well_count_df)
     all_well_count_df = enverus_df_to_gdf(all_well_count_df)
 
@@ -150,13 +163,13 @@ def task_get_ng_all_well_count_proxy_data(
         ifile_name = get_nei_file_name(nei_data_year, ng_well_count_file_names)
         nei_iyear = get_raw_NEI_data(iyear, nei_data_year, ifile_name)
         nei_df = pd.concat([nei_df, nei_iyear])
-    
+
     # Convert NEI Data to GDF and polygon to centroid point
     nei_df = gpd.GeoDataFrame(nei_df, crs=4326)
     nei_df = nei_df.to_crs(3857)  # projected CRS for centroid calculation
     nei_df.loc[:, 'geometry'] = nei_df.loc[:, 'geometry'].centroid
     nei_df = nei_df.to_crs(4326)
-    
+
     # Add NEI Data to Enverus Data
     all_well_count_df = pd.concat([all_well_count_df, nei_df]).reset_index(drop=True)
 
@@ -165,11 +178,15 @@ def task_get_ng_all_well_count_proxy_data(
     del nei_df
 
     # Check that relative emissions sum to 1.0 each state/year combination
-    sums = all_well_count_df.groupby(["state_code", "year"])["rel_emi"].sum()  # get sums to check normalization
-    assert np.isclose(sums, 1.0, atol=1e-8).all(), f"Relative emissions do not sum to 1 for each year and state; {sums}"  # assert that the sums are close to 1
+    sums = (
+        # get sums to check normalization
+        all_well_count_df.groupby(["state_code", "year"])["rel_emi"].sum()
+    )
+    # assert that the sums are close to 1
+    assert np.isclose(sums, 1.0, atol=1e-8).all(), f"Relative emissions do not sum to 1 for each year and state; {sums}"
 
     # Output Proxy Parquet Files
-    all_well_count_df = all_well_count_df.astype({'year':str})
+    all_well_count_df = all_well_count_df.astype({'year': str})
     all_well_count_df.to_parquet(all_well_count_output_path)
 
     return None
