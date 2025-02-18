7a55c294b82599cbc8bc81a49072c0e9c822cc6a
ccoxen <ccoxen@gmail.com>
2025-02-18 16:50:02 -0500

    Updated code to produce relative emissions by state and fixed misc pytask related code issues

--- gch4i/proxy_processing/task_anaerobic_digestion.py
+++ gch4i/proxy_processing/task_anaerobic_digestion.py
@@ -20,17 +20,13 @@ import numpy as np
 from pytask import Product, task, mark
 
 from gch4i.config import (
-    V3_DATA_PATH,
-    proxy_data_dir_path,
-    emi_data_dir_path,
-    years
+    proxy_data_dir_path
 )
 
 from gch4i.utils import (
-    create_final_proxy_df
+    normalize
 )
 
-anaerobic_digestion_emi_path = emi_data_dir_path / "anaerobic_digestion_emi.csv"
 anaerobic_digestion_proxy_path = proxy_data_dir_path / "anaerobic_digestion/AnaerobicDigestionFacilities.xlsx"
 anaerobic_digestion_proxy_ouput_path = proxy_data_dir_path / "anaerobic_digestion_proxy.parquet"
 
@@ -42,7 +38,6 @@ anaerobic_digestion_proxy_ouput_path = proxy_data_dir_path / "anaerobic_digestio
 @mark.persist
 @task(id='anaerobic_digestion_proxy')
 def task_anaerobic_digestion_proxy_data(
-    emi_path: Path = anaerobic_digestion_emi_path,
     proxy_path: Path = anaerobic_digestion_proxy_path,
     output_path: Annotated[Path, Product] = anaerobic_digestion_proxy_ouput_path
     ) -> None:
@@ -50,15 +45,12 @@ def task_anaerobic_digestion_proxy_data(
     This function processes the anaerobic digestion proxy data and calculates the proxy emissions.
 
     Args:
-    emi_path: Path to the anaerobic digestion emissions data.
     proxy_path: Path to the anaerobic digestion proxy data.
     output_path: Path to save the final proxy data.
 
     Returns:
     None. Proxy data is saved to a parquet file at the output_path.
     """
-    # Read in emi data
-    anaerobic_digestion_emi = pd.read_csv(anaerobic_digestion_emi_path)
 
     # %% Read in the proxy data
     # The anaerobic digestion facilities are from the Excess Food Opportunities Map Data (EPA)
@@ -67,7 +59,7 @@ def task_anaerobic_digestion_proxy_data(
     # download link: https://epa.maps.arcgis.com/home/item.html?id=d37cd4368d4d4ab8806546dd158c1d44
     # Within the subfolder 'ExcelTables', there is a file named 'AnaerobicDigestionFacilities.xlsx'
 
-    anaerobic_digestion_proxy = pd.read_excel(anaerobic_digestion_proxy_path, sheet_name='Data')
+    anaerobic_digestion_proxy = pd.read_excel(proxy_path, sheet_name='Data')
 
     # drop rows that are missing latitudes and longitudes - note that missing values appear to all be farm digesters
     anaerobic_digestion_proxy = anaerobic_digestion_proxy.dropna(subset=['Latitude', 'Longitude'])
@@ -84,78 +76,30 @@ def task_anaerobic_digestion_proxy_data(
     WRRF = 23587 / total_SCFM # 23,587 SCFM is from WRRF
     stand_alone = 4825 / total_SCFM # 4,825 SCFM is from stand-alone digesters
 
+    # Assign biogas production ratios to each facility type
+    anaerobic_digestion_proxy.loc[anaerobic_digestion_proxy['Facility Type'] == 'Stand-Alone', 'biogas'] = stand_alone
+    anaerobic_digestion_proxy.loc[anaerobic_digestion_proxy['Facility Type'] == 'Water Resource Recovery Facility', 'biogas'] = WRRF
 
-    # %% Calculate proxy emissions
-    final_proxy = pd.DataFrame()
-    for state in anaerobic_digestion_emi['state_code'].unique():
-        state_emi = anaerobic_digestion_emi[anaerobic_digestion_emi['state_code'] == state].copy()
-        state_proxy = anaerobic_digestion_proxy[anaerobic_digestion_proxy['State'] == state].copy()
+    # Rename columns
+    anaerobic_digestion_proxy = anaerobic_digestion_proxy.rename(columns={'Latitude': 'latitude', 'Longitude': 'longitude', 'State': 'state_code'})
 
-        for year in years:
-            state_emi_year = state_emi[state_emi['year'] == year]
-            state_proxy['year'] = year
+    # Create relative emissions for each state
+    anaerobic_digestion_proxy["rel_emi"] = anaerobic_digestion_proxy.groupby("state_code")["biogas"].transform(normalize)
 
-            WRRF_count = len(state_proxy[state_proxy['Facility Type'] == 'Water Resource Recovery Facility'])
-            stand_alone_count = len(state_proxy[state_proxy['Facility Type'] == 'Stand-Alone'])
+    # Check that the relative emissions sum to 1 for each state
+    sums = anaerobic_digestion_proxy.groupby(["state_code"])["rel_emi"].sum() #get sums to check normalization
+    assert np.isclose(sums, 1.0, atol=1e-8).all(), f"Relative emissions do not sum to 1 for each year and state; {sums}" # assert that the sums are close to 1
 
-            # Calculate proportional emissions if both facility types exist in the state
-            if stand_alone_count > 0 and WRRF_count > 0:
-                WRRF_emi = state_emi_year['ghgi_ch4_kt'].values[0] * WRRF
-                stand_alone_emi = state_emi_year['ghgi_ch4_kt'].values[0] * stand_alone
-                state_proxy.loc[state_proxy['Facility Type'] == 'Stand-Alone', 'emis_kt'] = stand_alone_emi / stand_alone_count
-                state_proxy.loc[state_proxy['Facility Type'] == 'Water Resource Recovery Facility', 'emis_kt'] = WRRF_emi / WRRF_count
-            
-            # Assign all emissions to WRRF if only WRRF facilities exist in the state
-            elif stand_alone_count == 0 and WRRF_count > 0:
-                state_proxy.loc[state_proxy['Facility Type'] == 'Water Resource Recovery Facility', 'emis_kt'] = state_emi_year['ghgi_ch4_kt'].values[0] / WRRF_count
-            
-            # Assign all emissions to stand-alone if only stand-alone facilities exist in the state (this never happens in the data but is included for completeness)
-            elif stand_alone_count > 0 and WRRF_count == 0:
-                state_proxy.loc[state_proxy['Facility Type'] == 'Stand-Alone', 'emis_kt'] = state_emi_year['ghgi_ch4_kt'].values[0] / stand_alone_count
+    # Create a GeoDataFrame and generate geometry from longitude and latitude
+    anaerobic_digestion_proxy = gpd.GeoDataFrame(
+        anaerobic_digestion_proxy,
+        geometry=gpd.points_from_xy(anaerobic_digestion_proxy['longitude'], anaerobic_digestion_proxy['latitude'], crs='EPSG:4326')
+    )
 
-            final_proxy = pd.concat([final_proxy, state_proxy], ignore_index=True)
-
-    # %% Create the final proxy df
-
-    def create_final_proxy_df(proxy_df):   
-        """
-        Function to create the final proxy df that is ready for gridding
-
-        Parameters:
-        - proxy_df: DataFrame containing proxy data.
-
-        Returns:
-        - final_proxy_df: DataFrame containing the processed emissions data for each state.
-        """
-        
-        # Create a GeoDataFrame and generate geometry from longitude and latitude
-        proxy_gdf = gpd.GeoDataFrame(
-            proxy_df,
-            geometry=gpd.points_from_xy(proxy_df['longitude'], proxy_df['latitude'], crs='EPSG:4326')
-        )
-
-        # subset to only include the columns we want to keep
-        proxy_gdf = proxy_gdf[['state_code', 'year', 'emis_kt', 'geometry']]
-        
-        # Normalize relative emissions to sum to 1 for each year and state
-        proxy_gdf = proxy_gdf.groupby(['state_code', 'year']).filter(lambda x: x['emis_kt'].sum() > 0) #drop state-years with 0 total volume
-        proxy_gdf['rel_emi'] = proxy_gdf.groupby(['year', 'state_code'])['emis_kt'].transform(lambda x: x / x.sum() if x.sum() > 0 else 0) #normalize to sum to 1
-        sums = proxy_gdf.groupby(["state_code", "year"])["rel_emi"].sum() #get sums to check normalization
-        assert np.isclose(sums, 1.0, atol=1e-8).all(), f"Relative emissions do not sum to 1 for each year and state; {sums}" # assert that the sums are close to 1
-        
-        # Drop the original emissions column
-        proxy_gdf = proxy_gdf.drop(columns=['emis_kt'])
-
-        # Rearrange columns
-        proxy_gdf = proxy_gdf[['state_code', 'year', 'rel_emi', 'geometry']]
-
-        return proxy_gdf
-
-    final_proxy = final_proxy.rename(columns={'Latitude': 'latitude', 'Longitude': 'longitude', 'State': 'state_code'})
-
-    final_proxy = create_final_proxy_df(final_proxy)
+    # Only keep the columns we need
+    anaerobic_digestion_proxy = anaerobic_digestion_proxy[['state_code', 'rel_emi', 'geometry']]
 
     # %% Save the final proxy dataframes to parquet files
-    final_proxy.to_parquet(anaerobic_digestion_proxy_ouput_path, index=False)
+    anaerobic_digestion_proxy.to_parquet(output_path, index=False)
 
 # %%
