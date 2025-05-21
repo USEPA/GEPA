250d66eecc1ac797a83cd4f1dc132821df30968b
nkruskamp <nkruskamp@rti.org>
2025-05-21 13:01:47 -0400

    fixed to composting for order of adding facilities

--- gch4i/proxy_processing/task_composting_proxy.py
+++ gch4i/proxy_processing/task_composting_proxy.py
@@ -41,7 +41,7 @@ from gch4i.utils import name_formatter, normalize
 
 # %% Assign Constant Variables
 COMPOSTING_FRS_NAICS_CODE = 562219
-DUPLICATION_TOLERANCE_M = 250
+DUPLICATION_TOLERANCE_M = 2_500  # 2.5 km
 
 composting_dir = sector_data_dir_path / "composting"
 # %%
@@ -91,105 +91,151 @@ def get_composting_proxy_data(
         .to_crs(4326)
     )
 
-  #the logic for this section is that we want to prioritize EPA facility location data. Next prioritize biocycle and composting council, and then fill in remaining gaps with FRS 
-  # First, we need to remove duplicates within 250 m within the EPA data set
-    # read in the composting facilities data
-    excess_food_df = (
-        pd.read_excel(
-            excess_food_op_path,
-            sheet_name="Data",
-            usecols=["Name", "Latitude", "Longitude"],
-        ).rename(columns=str.lower)
-        .assign(
-            formatted_fac_name=lambda df: name_formatter(df["name"]), source="ex_food"
+    # This geom will be used to filter facilities spatially
+    STATE_UNION = state_gdf.to_crs("ESRI:102003").union_all()
+    # %%
+
+    # read in facilities data from all the sources
+
+    def make_gdf(in_df):
+        out_gdf = gpd.GeoDataFrame(
+            in_df.drop(columns=["latitude", "longitude"]),
+            geometry=gpd.points_from_xy(in_df["longitude"], in_df["latitude"]),
+            crs=4326,
+        ).to_crs("ESRI:102003")
+        out_gdf = out_gdf[out_gdf.is_valid & ~out_gdf.is_empty]
+        out_gdf = out_gdf[out_gdf.intersects(STATE_UNION)].loc[
+            :, ["source", "geometry"]
+        ]
+        return out_gdf
+
+    def read_comp_coucil_data(in_path):
+        # google earth exports 3d points, covert them to 2d to avoid issues.
+        def _drop_z(geom):
+            return wkb.loads(wkb.dumps(geom, output_dimension=2))
+
+        out_gdf = (
+            gpd.read_file(in_path, driver="KML")
+            .rename(columns=str.lower)
+            .drop(columns="description")
+            .assign(
+                formatted_fac_name=lambda df: name_formatter(df["name"]),
+                geometry=lambda df: df.geometry.apply(_drop_z),
+                source="comp_council",
+            )
+            .to_crs("ESRI:102003")
         )
-    )
 
-  # for both the biocycle and composting council datasets, we have less confidence in the location data, therefore,
-  # we want to compare the locations of facilities with the EPA data and remove any facilities that are within 2.5 km of an EPA facility, 
-    # read in the biocycle composting facilities data
-    biocycle_df = (
-        pd.read_csv(biocycle_path)
-        .rename(columns={"lat": "latitude", "lon": "longitude"})
-        .assign(source="biocycle")
-    )
+        out_gdf = out_gdf[out_gdf.is_valid & ~out_gdf.is_empty]
+        out_gdf = out_gdf[out_gdf.intersects(STATE_UNION)].loc[
+            :, ["source", "geometry"]
+        ]
 
-    # google earth exports 3d points, covert them to 2d to avoid issues.
-    def _drop_z(geom):
-        return wkb.loads(wkb.dumps(geom, output_dimension=2))
+        return out_gdf
 
-    # read in the compost council composting facilities data
-    comp_council_gdf = (
-        gpd.read_file(comp_council_path, driver="KML")
-        .rename(columns=str.lower)
-        .drop(columns="description")
-        .assign(
-            formatted_fac_name=lambda df: name_formatter(df["name"]),
-            geometry=lambda df: df.geometry.apply(_drop_z),
-            source="comp_council",
+    def read_biocycle_data(in_path):
+        out_df = (
+            pd.read_csv(in_path)
+            .rename(columns={"lat": "latitude", "lon": "longitude"})
+            .assign(source="biocycle")
         )
-    )
+        out_gdf = make_gdf(out_df)
+        return out_gdf
 
-    # Last, read in the FRS data and remove and duplicate facilities within 250 m from within this data set,
-  # then, compare against the other three datasets and remove any facilities that are within 2.5km of any other facilities. 
-  
-    # get the composting facilities from the FRS database based on the NAICS code.
-    # I use duckdb for a bit of performance in these very large tables over pandas.
-
-  # In v2, we also read in the 'Collect_Desc' column and filtered the data for all "collect_Desc' != 'INTERPOLATION-OTHER'. We should make sure that is implemented here too
-    frs_composting_fac_df = (
-        duckdb.execute(
-            (
-                "SELECT frs_main.primary_name as name, frs_main.latitude83 as latitude, frs_main.longitude83 as longitude "  # noqa
-                f"FROM (SELECT registry_id, primary_name, latitude83, longitude83 FROM '{frs_facility_path}') as frs_main "  # noqa
-                f"JOIN (SELECT registry_id, naics_code FROM '{frs_naics_path}') AS frs_naics "  # noqa
-                "ON frs_main.registry_id = frs_naics.registry_id "
-                f"WHERE naics_code == {COMPOSTING_FRS_NAICS_CODE}"
+    def read_frs_data(frs_path, naics_path):
+        out_df = (
+            duckdb.execute(
+                (
+                    "SELECT frs_main.primary_name as name, frs_main.latitude83 as latitude, frs_main.longitude83 as longitude "  # noqa
+                    f"FROM (SELECT registry_id, primary_name, latitude83, longitude83 FROM '{frs_path}') as frs_main "  # noqa
+                    f"JOIN (SELECT registry_id, naics_code FROM '{naics_path}') AS frs_naics "  # noqa
+                    "ON frs_main.registry_id = frs_naics.registry_id "
+                    f"WHERE naics_code == {COMPOSTING_FRS_NAICS_CODE}"
+                )
+            )
+            .df()
+            .assign(
+                formatted_fac_name=lambda df: name_formatter(df["name"]), source="frs"
             )
         )
-        .df()
-        .assign(formatted_fac_name=lambda df: name_formatter(df["name"]), source="frs")
-    )
+        out_gdf = make_gdf(out_df)
 
-  # lastly, after filtering for duplicates, all four datasets can be combined into a single dataframe
-  
-    # there is a two step process to get all facilities in one dataframe:
-    # 1) put together the facilities that have lat/lon columns and make them
-    # geodataframes,
-    facility_concat_df = pd.concat(
-        [frs_composting_fac_df, biocycle_df, excess_food_df]
-    ).sort_values("formatted_fac_name")
-    facility_concat_gdf = gpd.GeoDataFrame(
-        facility_concat_df.drop(columns=["latitude", "longitude"]),
-        geometry=gpd.points_from_xy(
-            facility_concat_df["longitude"], facility_concat_df["latitude"]
-        ),
-        crs=4326,
-    )
-    # 2) concat these with the facilities data that are already geodataframes.
-    facility_concat_2_gdf = pd.concat([comp_council_gdf, facility_concat_gdf])
-    facility_concat_2_gdf = facility_concat_2_gdf[
-        facility_concat_2_gdf.is_valid & ~facility_concat_2_gdf.is_empty
-    ]
-    facility_concat_2_gdf.normalize()
+        return out_gdf
 
-    # Remove duplicate facilities based on our spatial tolerance
-    # if two or more facilities fall within the tolerance, only 1 location is kept, the
-    # resulting centroid will be at the intersection of their buffers.
-    final_facility_gdf = (
-        facility_concat_2_gdf.to_crs("ESRI:102003")
-        .assign(geometry=lambda df: df.buffer(DUPLICATION_TOLERANCE_M)) # removing facilities at this step does not filter out as many duplicate facilities as the above recommended approach
-        # also, how does the above filter work? i.e., within a 250m radius, which facility does this keep and which does it remove? Since this is a different method than v2, I'm wondering if there are two facilities in neighboring grid cells and the v2 code keeps facility #1, whether this code is keeping facility #2.
-      # I'm noticing some red and blue grid cells next to each other in the v2/v3 comparison map, so I'm just speculating wh ythat would be the case. 
-        .dissolve()
-        .explode()
-        .centroid.to_frame()
-        .to_crs(4326)
-        .sjoin(state_gdf[["geometry", "state_code"]])
-        .drop(columns="index_right")
-        .rename_geometry("geometry")
-    )
+    def read_excess_food_data(in_path):
+        out_df = (
+            pd.read_excel(
+                in_path,
+                sheet_name="Data",
+                usecols=["Name", "Latitude", "Longitude"],
+            )
+            .rename(columns=str.lower)
+            .assign(
+                formatted_fac_name=lambda df: name_formatter(df["name"]),
+                source="ex_food",
+            )
+        )
+        out_gdf = make_gdf(out_df).loc[:]
+
+        return out_gdf
+
+    epa_gdf = read_excess_food_data(excess_food_op_path)
+    biocycle_gdf = read_biocycle_data(biocycle_path)
+    frs_gdf = read_frs_data(frs_facility_path, frs_naics_path)
+    comp_council_gdf = read_comp_coucil_data(comp_council_path)
     # %%
+    # plot out the data to see what we have
+    ax = state_gdf.to_crs("ESRI:102003").boundary.plot(color="black", linewidth=0.5)
+    epa_gdf.plot(ax=ax, color="blue", markersize=5, label="EPA Facilities")
+    biocycle_gdf.plot(ax=ax, color="red", markersize=5, label="Biocycle Facilities")
+    comp_council_gdf.plot(
+        ax=ax, color="green", markersize=5, label="Composting Council Facilities"
+    )
+    frs_gdf.plot(ax=ax, color="orange", markersize=5, label="FRS Facilities")
+    ax.set_title("Composting Facilities")
+
+    # %%
+
+    # for both the biocycle and composting council datasets, we have less confidence in
+    # the location data, therefore, we want to compare the locations of facilities with
+    # the EPA data and remove any facilities that are within 2.5 km of an EPA facility.
+
+    # We give first preference to the EPA data, so we first define the final faclity
+    # list with it.
+    final_fac_gdf = epa_gdf.copy()
+    print(f"starting fac count:            {len(final_fac_gdf)}")
+    print("=" * 40)
+
+    # remove any facilities that are within 2.5 km of an EPA facility and the cascading
+    # join of adding new facilities to the final dataset. The order here gives
+    # preference to the EPA data, then biocycle, then composting council, and finally
+    # FRS.
+    fac_buffer = final_fac_gdf.buffer(DUPLICATION_TOLERANCE_M).union_all()
+    for other_gdf in [biocycle_gdf, comp_council_gdf, frs_gdf]:
+        source = other_gdf["source"].iloc[0]
+        print(f"processing source:             {source}")
+        print("-" * 40)
+        print(f"facility count:                {len(other_gdf)}")
+        # check if the other gdf intersects with the final facilities data.
+        intersect_mask = other_gdf.intersects(fac_buffer)
+        # get the number of facilities that DO intersect with the EPA buffer
+        # and report their count, these will not be used.
+        drop_gdf = other_gdf[intersect_mask]
+        # get the facilities that are outside the buffer and should be added to the
+        # final_fac_gdf
+        add_gdf = other_gdf[~intersect_mask]
+
+        print(f"duplicate facility count:      {len(drop_gdf)}")
+        print(f"adding new facility count:     {len(add_gdf)}")
+        # add the intersecting facilities to the final_fac_gdf
+        final_fac_gdf = pd.concat([final_fac_gdf, add_gdf])
+        print("-" * 40)
+        print(f"new total proxy facilities:    {len(final_fac_gdf)}")
+        print()
+        # create a new buffer for the final_fac_gdf
+        fac_buffer = final_fac_gdf.buffer(DUPLICATION_TOLERANCE_M).union_all()
+    print("=" * 40)
+
     # NOTE: DC has no facilities listed, but has 1 year (2012) with reported emissions.
     # Erin email note on DC: My understanding is that (industrial) composting in DC has
     # been outsources to MD. Since the inventory is trying to capture the distribution
@@ -197,23 +243,30 @@ def get_composting_proxy_data(
     # for DC. Let’s use the Fort Totten Waste Transfer Station (lat/lon: 38.947624,
     # -77.001213). We can make a note of this in the data and assumptions document and
     # this assumption can be re-visited in later iterations.
+    dc_gdf = gpd.GeoDataFrame(
+        {"source": "manual", "geometry": [Point(-77.001213, 38.947624)]}, crs=4326
+    )
+
+    print("adding DC facility")
+    print("=" * 40)
+    # add the DC facility and join the state_code onto the facilities
     proxy_gdf = (
-        pd.concat(
-            [
-                final_facility_gdf,
-                state_gdf[state_gdf["state_code"] == "DC"][
-                    ["state_code", "geometry"]
-                ].assign(geometry=Point(-77.001213, 38.947624)),
-            ]
-        )
+        pd.concat([final_fac_gdf.to_crs(4326), dc_gdf])
         .reset_index(drop=True)
         .assign(emis_kt=1)
+        .sjoin(state_gdf[["state_code", "geometry"]])
+        .drop(columns="index_right")
     )
+    print(f"final fac count:               {len(proxy_gdf)}")
+    # %%
+    # normalize the emiissions by equal allocation to all facilites within a state
+    # Since there is no year to these data, there is no yearly normalization applied.
     proxy_gdf["rel_emi"] = proxy_gdf.groupby("state_code")["emis_kt"].transform(
         normalize
     )
-    print("composting facilities with location: ", len(proxy_gdf))
 
+    # check that the emissions are normalized correctly
+    # this should be 1 for all states
     all_eq_df = (
         proxy_gdf.groupby("state_code")["rel_emi"]
         .sum()
@@ -221,10 +274,16 @@ def get_composting_proxy_data(
         .to_frame()
         .assign(is_close=lambda df: (np.isclose(df["sum_check"], 1)))
     )
-    all_eq_df
 
     if not all_eq_df["is_close"].all():
         raise ValueError("not all values are normed correctly!")
+    # %%
+    # final plot of the data by source
+    ax = state_gdf.boundary.plot(color="xkcd:slate", linewidth=0.5)
+    proxy_gdf.plot(
+        "source", cmap="Set3", markersize=1, categorical=True, legend=True, ax=ax
+    )
+    ax.set_title("Composting proxy Facilities")
     # %%
     proxy_gdf.to_parquet(dst_path)
 
