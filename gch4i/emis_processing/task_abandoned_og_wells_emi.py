f14256db87f02420a0e92e2aefb961b1bdf0d7cc
nkruskamp <nkruskamp@rti.org>
2025-06-06 09:46:54 -0400

    updated input data to v3 files, bug fixes, refactoring, add scaling of well plug status

--- gch4i/emis_processing/task_abandoned_og_wells_emi.py
+++ gch4i/emis_processing/task_abandoned_og_wells_emi.py
@@ -10,6 +10,7 @@ Output Files:           - {emi_data_dir_path}/
                             aog_gas_wells_emi.csv
                             aog_oil_wells_emi.csv
 """
+
 # %% STEP 0. Load packages, configuration files, and local parameters ------------------
 from pathlib import Path
 from typing import Annotated
@@ -23,7 +24,7 @@ from gch4i.config import (
     emi_data_dir_path,
     ghgi_data_dir_path,
     max_year,
-    min_year
+    min_year,
 )
 from gch4i.utils import tg_to_kt
 
@@ -54,7 +55,7 @@ def get_abandoned_og_wells_inv_data(in_path, src, params):
         sheet_name=params["arguments"][0],  # sheet name
         skiprows=params["arguments"][1],  # skip rows
         nrows=params["arguments"][2],  # number of rows
-        )
+    )
     # Specify years to keep
     year_list = [str(x) for x in list(range(min_year, max_year + 1))]
     # Clean and format the data
@@ -92,7 +93,7 @@ def get_abandoned_og_wells_inv_data(in_path, src, params):
         .groupby(["state_code", "year"])["ghgi_ch4_kt"]
         .sum()
         .reset_index()
-        )
+    )
     return emi_df
 
 
@@ -124,7 +125,7 @@ for emi_name, data in proxy_data.groupby("emi_id"):
         "input_paths": [ghgi_data_dir_path / source_path / x for x in data.file_name],
         "source_list": [x.strip().casefold() for x in data.Subcategory2.to_list()],
         "parameters": ast.literal_eval(data.add_params.iloc[0]),
-        "output_path": emi_data_dir_path / f"{emi_name}.csv"
+        "output_path": emi_data_dir_path / f"{emi_name}.csv",
     }
 
 emi_parameters_dict
@@ -132,32 +133,43 @@ emi_parameters_dict
 
 # %% STEP 3. Create Pytask Function and Loop
 
-for _id, _kwargs in emi_parameters_dict.items():
 
-    @mark.persist
-    @task(id=_id, kwargs=_kwargs)
-    def task_abandoned_og_wells_emi(
-        input_paths: list[Path],
-        source_list: list[str],
-        parameters: dict,
-        output_path: Annotated[Path, Product],
-    ) -> None:
+def task_abandoned_og_wells_emi(
+    input_paths: list[Path],
+    source_list: list[str],
+    parameters: dict,
+    output_path: Annotated[Path, Product],
+) -> None:
 
-        # Initialize the emi_df_list
-        emi_df_list = []
-        # Loop through the input paths and source list to get the emissions data
-        for input_path, ghgi_group in zip(input_paths, source_list):
-            individual_emi_df = get_abandoned_og_wells_inv_data(input_path,
-                                                                ghgi_group,
-                                                                parameters)
-            emi_df_list.append(individual_emi_df)
-
-        # Concatenate the emissions data and group by state and year
-        emission_group_df = (
-            pd.concat(emi_df_list)
-            .groupby(["state_code", "year"])["ghgi_ch4_kt"]
-            .sum()
-            .reset_index()
+    # Initialize the emi_df_list
+    emi_df_list = []
+    # Loop through the input paths and source list to get the emissions data
+    for input_path, ghgi_group in zip(input_paths, source_list):
+        individual_emi_df = get_abandoned_og_wells_inv_data(
+            input_path, ghgi_group, parameters
         )
-        # Save the emissions data to the output path
-        emission_group_df.to_csv(output_path)
+        emi_df_list.append(individual_emi_df)
+
+    # Concatenate the emissions data and group by state and year
+    emission_group_df = (
+        pd.concat(emi_df_list)
+        .groupby(["state_code", "year"])["ghgi_ch4_kt"]
+        .sum()
+        .reset_index()
+    )
+    # Save the emissions data to the output path
+    emission_group_df.to_csv(output_path)
+
+
+# %%
+import pytask
+
+sesh = pytask.build(
+    tasks=[
+        task_abandoned_og_wells_emi(**kwargs)
+        for _id, kwargs in emi_parameters_dict.items()
+    ]
+)
+sesh
+
+# %%
--- gch4i/run_whole_group.py
+++ gch4i/run_whole_group.py
@@ -42,11 +42,13 @@ g_info = GriddingInfo(update_mapping=True, save_file=True)
 g_info.display_all_pair_statuses()
 # %%
 
-gch4i_name = "5B1_composting"
+gch4i_name = "1B2ab_abandoned_og_wells"
 
 gridding_rows = g_info.pairs_ready_for_gridding_df.query(
     f"gch4i_name == '{gch4i_name}'"
 )
+gridding_rows
+# %%
 for emi_proxy_data in tqdm(
     gridding_rows.itertuples(index=False),
     total=len(gridding_rows),
